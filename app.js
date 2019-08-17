f27f07307ff11704fbf8c8fdea0d4c9b56aab1df
Apisit Tanapichartsakul <title.apt@hotmail.com>
2019-08-17 11:43:16 +0700

    Project Pokemon week2

--- app.js
+++ app.js
@@ -35,9 +35,29 @@ app.get('/pokemon/:id' ,(req, res) => {
     let p = pokemons[id - 1]
     res.send(p)
 })
+// POST pokemons -> add pokemon list
+app.post('/pokemon', (req, res) => {
+
+    if(req.body.name === undefined 
+        || req.body.name === null 
+        || req.body.name === '' 
+        || req.body.type === undefened 
+        || req.body.type === null 
+        || req.body.type === ''){ // ต้องป้อน name & type
+        
+        res.status(400).send('Insufficient parameters: name and type are required parameter')
+        return
+    }
+
+    let p = new Pokemons(req.body.name , req.body.type)
+    p.id = generateId()
+    console.log(req.body)
+    pokemons.push(p)
+    res.sendStatus(201)
+})
 
 app.put('/pokemon/:id', (req , res) => {
-    
+
     if(!isSufficienParam(req.body.type2)){
         res.status(400).send({ error:'Insufficient parameters: name and type are required parameter'})
         return
@@ -60,29 +80,21 @@ app.put('/pokemon/:id', (req , res) => {
     res.sendStatus(201)
 })
 
-
-
-// POST pokemons -> add pokemon list
-app.post('/pokemon', (req, res) => {
-
-    if(req.body.name === undefined 
-        || req.body.name === null 
-        || req.body.name === '' 
-        || req.body.type === undefened 
-        || req.body.type === null 
-        || req.body.type === ''){ // ต้องป้อน name & type
-        
-        res.status(400).send('Insufficient parameters: name and type are required parameter')
+app.delete('/pokemon/:id', (req , res) => {
+    if(!isSufficienParam(req.param.id)){
+        res.status(400).send({ error:'Insufficient parameters: name and type are required parameter'})
         return
     }
 
-    let p = new Pokemons(req.body.name , req.body.type)
-    p.id = generateId()
-    console.log(req.body)
-    pokemons.push(p)
-    res.sendStatus(201)
+    let p = pokemons[id - 1]
+    if(p === undefined){
+        res.status(400).send({ error:'Cannot update Pokemon: Pokemo is not found'})
+        return
+    }
+
+    delete pokemons[id - 1]
+    res.sendStatus(204)
+
 })
-
-
 app.listen(port, () => console.log(`Pokemons app listening on port ${port}!`)) //ให้แอปเริมทำงาน
 
